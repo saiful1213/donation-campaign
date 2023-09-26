97f9a1c3460f8485a0bd025e6e7cc45721fc661b
saiful-islam <md2215566@gmail.com>
2023-09-26 23:56:41 +0600

    search category implement

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -1,5 +1,12 @@
+/* eslint-disable react/prop-types */
 
-const Banner = () => {
+const Banner = ({handleSearch}) => {
+   
+   const handleInput = () =>{
+      const inputValue = document.getElementById('inputField').value;
+      return inputValue
+   }
+
    return (
       <div className="mt-12 p-2 h-[60vh] flex justify-center items-center bg-bannerImg bg-cover bg-center bg-no-repeat">
          <div className="">
@@ -8,8 +15,8 @@ const Banner = () => {
             <div className="flex justify-center items-center">
                <div className="form-control mt-10">
                   <label className="input-group">
-                     <input type="text" placeholder="Search here...." className="input focus:outline-none input-bordered" />
-                     <span className="bg-[#FF444A] font-semibold text-white">Search</span>
+                     <input type="text" id="inputField" onChange={()=>handleInput()} placeholder="Search here by category...." className="input focus:outline-none input-bordered" />
+                     <span className="bg-[#FF444A] font-semibold text-white" onClick={()=>handleSearch(handleInput)}>Search</span>
                   </label>
                </div>
             </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,41 @@
+/* eslint-disable no-unused-vars */
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../Components/Header/Banner/Banner";
 import Cards from "../../Components/Cards/Cards";
+import { useState } from "react";
 
 const Home = () => {
-   const data = useLoaderData();
+   const allData = useLoaderData();
+   const [data, setData] = useState(allData);
+
+   const handleSearch = value =>{
+      const fieldValue = value();
+
+      if(fieldValue.toLowerCase() === 'health'){
+         const healthCtg = data.filter(obj => obj.category.includes('Health'));
+         setData(healthCtg);
+      }
+      else if(fieldValue.toLowerCase() === 'education'){
+         const educationCtg = allData.filter(obj => obj.category.includes('Education'));
+         setData(educationCtg);
+      }
+      else if(fieldValue.toLowerCase() === 'clothing'){
+         const clothingCtg = allData.filter(obj => obj.category.includes('Clothing'));
+         setData(clothingCtg);
+      }
+      else if(fieldValue.toLowerCase() === 'food'){
+         const foodCtg = allData.filter(obj => obj.category.includes('Food'));
+         setData(foodCtg);
+      }
+      else if(fieldValue.toLowerCase() === 'all'){
+         setData(allData);
+      }
+   } 
+
+
    return (
       <div>
-         <Banner></Banner>
+         <Banner handleSearch={handleSearch}></Banner>
          <div className="p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-12">
             {
                data.map(card => <Cards key={card.id} card={card}></Cards>)
